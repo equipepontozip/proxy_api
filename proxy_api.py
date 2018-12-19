7f2a7ecf123e6872813683fea114ac52686ae14c
matheusbsilva <matheusbattista@hotmail.com>
2018-12-19 18:22:20 -0200

    initial proxy of original api response

--- proxy_api.py
+++ proxy_api.py
@@ -1,8 +1,14 @@
 from flask import Flask
+from flask import jsonify
+import requests
 
 app = Flask(__name__)
+ORIGINAL_URL='http://00224.transdatasmart.com.br:22401/ITS-infoexport/api/Data/VeiculosGTFS'
 
 @app.route("/")
-def hello():
-    return "Hello World"
+def proxy():
+    original_resp = requests.get(ORIGINAL_URL)
+    response = jsonify(original_resp.json())
+    response.status_code = original_resp.status_code
 
+    return response
