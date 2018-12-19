d60048d9fe5b065a88cdd7632162d74ed466fb7b
matheusbsilva <matheusbattista@hotmail.com>
2018-12-19 18:39:45 -0200

    refact the way to get the data

--- proxy_api.py
+++ proxy_api.py
@@ -1,14 +1,26 @@
 from flask import Flask
 from flask import jsonify
-import requests
+from urllib import request
+import json
 
 app = Flask(__name__)
 ORIGINAL_URL='http://00224.transdatasmart.com.br:22401/ITS-infoexport/api/Data/VeiculosGTFS'
 
+def get_data():
+    req = request.Request(ORIGINAL_URL, method='GET')
+    app.logger.info('Requesting on %s' % ORIGINAL_URL)
+
+    response = request.urlopen(req)
+    body = response.read().decode('utf-8')
+
+    js = json.loads(body)
+
+    return js
+
 @app.route("/")
 def proxy():
-    original_resp = requests.get(ORIGINAL_URL)
-    response = jsonify(original_resp.json())
-    response.status_code = original_resp.status_code
+
+    original_resp = get_data()
+    response = jsonify(original_resp)
 
     return response
