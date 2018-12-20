0e64c6af3caca534e14f67336798413ffd5d9412
chris-redfield <christianmoryah@gmail.com>
2018-12-20 18:13:07 -0200

    add arquivos do front - mais facil

--- proxy_api.py
+++ proxy_api.py
@@ -3,9 +3,9 @@ from urllib import request
 
 import pandas as pd
 from flask import Flask
-from flask import jsonify
+from flask import jsonify, render_template
 
-app = Flask(__name__)
+app = Flask(__name__,template_folder='./static')
 ORIGINAL_URL='http://00224.transdatasmart.com.br:22401/ITS-infoexport/api/Data/VeiculosGTFS'
 
 def get_data():
@@ -35,10 +35,21 @@ def process_data(df):
 
     return df
 
-@app.route("/")
+@app.route('/')
+def root():
+    #return json.dumps({'success':True}), 200, {'ContentType':'application/json'} 
+    return render_template('index.html')
+    
+
+
+@app.route("/data")
 def proxy():
 
     original_resp = get_data()
     response = jsonify(original_resp)
 
     return response
+
+@app.route('/teste')
+def teste():
+    return json.dumps({'success':True}), 200, {'ContentType':'application/json'} 
